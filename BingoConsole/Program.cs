b89aeda93071d8a4d791f19512acae9b3f80482a
HZNFSheikh <hsheikh@spartaglobal.com>
2018-08-20 04:05:37 +0100

    Commit with number generator and players numbers

--- BingoConsole/Program.cs
+++ BingoConsole/Program.cs
@@ -4,12 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BingoConsole
+namespace BingoConsoleGame
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            //Player1 chooses their numbers
+            List<int> arr = new List<int>();
+            Console.WriteLine("Player 1 choose your numbers");
+
+
+            for (int i = 0; i < 6; i++)
+                arr.Add(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine(" Player 1 Your numbers are \n");
+
+            foreach (int v in arr)
+                Console.WriteLine("{0}\n", v);
+            int[] playerNums = arr.ToArray();
+
+
+            //Generate 99 random numbers to be called out
+            Random randomNumber = new Random();
+
+            int[] bingoNumber = Enumerable
+                .Repeat(0, 99)
+                .Select(i => randomNumber.Next(1, 99))
+                .ToArray();
+
+            Console.WriteLine(bingoNumber);
+
+            foreach (var entry in bingoNumber)
+            {
+                Console.WriteLine("The chosen number is " + entry);
+            }
+
+            for (int i = 0; i < bingoNumber.Length; i++)
+            {
+                for (int j = 0; j < playerNums.Length; j++)
+                {
+                    if (bingoNumber[i] == playerNums[j])
+                    {
+                        Console.WriteLine(bingoNumber[i] + " is a match");
+                    }
+                }
+            }
+
+
         }
     }
 }
